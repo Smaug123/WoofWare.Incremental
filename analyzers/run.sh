abd552617568fda9f8f9de754041d197184f41e8
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2025-10-03 13:36:27 +0100

    Fix

--- analyzers/run.sh
+++ analyzers/run.sh
@@ -4,14 +4,10 @@ nix develop --command dotnet build || exit 1
 nix develop --command dotnet restore analyzers/analyzers.fsproj || exit 1
 nix build .#fsharp-analyzers || exit 1
 
-echo "GITHUB: $GITHUB_WORKSPACE"
-echo "PWD: $(pwd)"
-ls -la
-
-nix run .#fsharp-analyzers -- --verbosity detailed --analyzers-path ./.analyzerpackages/G-Research.FSharp.Analyzers/*/ --project WoofWare.Incremental/WoofWare.Incremental.fsproj --report gr.sarif
+nix run .#fsharp-analyzers -- --verbosity detailed --analyzers-path ./.analyzerpackages/g-research.fsharp.analyzers/*/ --project WoofWare.Incremental/WoofWare.Incremental.fsproj --report gr.sarif
 exit_code=$?
 
-nix run .#fsharp-analyzers -- --verbosity detailed --analyzers-path ./.analyzerpackages/WoofWare.FSharpAnalyzers/*/ --project WoofWare.Incremental/WoofWare.Incremental.fsproj --report woofware.sarif
+nix run .#fsharp-analyzers -- --verbosity detailed --analyzers-path ./.analyzerpackages/woofware.fsharpanalyzers/*/ --project WoofWare.Incremental/WoofWare.Incremental.fsproj --report woofware.sarif
 exit_code_2=$?
 
 if [ "$exit_code" -eq 0 ]; then
